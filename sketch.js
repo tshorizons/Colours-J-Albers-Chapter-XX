d4ab8e9b685d94d9d44c29d229dc41dc66e6a38d
tshorizons <123700523+tshorizons@users.noreply.github.com>
2023-09-19 18:21:54 +0100

    Change location of rectangles

--- sketch.js
+++ sketch.js
@@ -4,27 +4,27 @@ let w=2;
 let s=0;
 
 function setup() {
-  createCanvas(400, 600);
+  createCanvas(400, 400);
   noLoop();
 }
 
 function draw() {
   colorMode(HSB,360,128,100);
-  background(180,128,100);
+  background(240,128,100);
 
 
   for(let i=0; i<400;i=i+40){
     console.log(i);
     noStroke()
-    fill(0,128,100-w);
-    rect(0,i,150,40);
+    fill(21,128,100-w);
+    rect(20,i,150,40);
     w=w+2;
 }
 for(let i=0; i<400;i=i+40){
   console.log(i);
   noStroke()
   fill(0,128,100-z);
-  rect(200,i,150,42);
+  rect(230,i,150,42);
   s=s+1;
   z=2**(s);
 }
